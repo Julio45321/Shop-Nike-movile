b6398c01d7cdd54a43d17cc1bd9d0fff4e866427
Cesar <julio45321@gmail.com>
2023-08-15 13:18:27 -0600

    finalizado

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_nike/models/cart.dart';
+
+import '../models/shoe.dart';
+import 'cart_item.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('card'),);
+    return Consumer<Cart>(
+      builder: (context, value, child) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'My Carrito',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+             SizedBox(
+              height: 20,
+            ),
+
+            Expanded(
+              child:
+               ListView.builder(
+                itemCount: value.getUserCart().length,
+                itemBuilder:(context, index) {
+               
+               // obtener compra individual
+                  Shoe individualShoe = value.getUserCart()[index];
+
+                  // regresar item al carrito
+                  return CartItem( shoe: individualShoe,);
+           },
+           ),
+           )
+          ],
+        ),
+      ),
+    );
   }
 }
